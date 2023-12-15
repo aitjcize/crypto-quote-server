ec6eeb2d668f9fd0e6402eecea65808cb6b411b6
Wei-Ning Huang <aitjcize@gmail.com>
2023-12-16 02:20:22 +0800

    quote: fix return value parsing and tests

--- src/quote.rs
+++ src/quote.rs
@@ -4,7 +4,6 @@ use std::io::{Error as IoError, ErrorKind};
 use reqwest::{self, StatusCode};
 use rust_decimal::prelude::Decimal;
 use serde_json::{self, Value};
-use terra_rust_api::Terra;
 
 async fn fetch_price_from_coingecko(ids: &[&str]) -> Result<Vec<Decimal>, Box<dyn Error>> {
     const COINGECKO_API_TMPL: &str =
@@ -28,36 +27,19 @@ async fn fetch_price_from_coingecko(ids: &[&str]) -> Result<Vec<Decimal>, Box<dy
         if let Value::Null = value[id]["usd"] {
             return Decimal::new(0, 0);
         }
-        Decimal::from_str_radix(&value[id]["usd"].to_string(), 10).unwrap()
+        let mut result = Decimal::from_str_radix(&value[id]["usd"].to_string(), 10);
+        if result.is_err() {
+            result = Decimal::from_scientific(&value[id]["usd"].to_string());
+        }
+        result.unwrap()
     }).collect();
 
     Ok(result)
 }
 
-async fn fetch_price_aust() -> Result<Decimal, Box<dyn Error>> {
-    let anchor_overseer_address = "terra1tmnqgvg567ypvsvk6rwsga3srp7e3lg6u0elp8";
-    let query = "%7B%22epoch_state%22%3A%7B%7D%7D";
-
-    let client = Terra::lcd_client_no_tx("https://fcd.terra.dev", "columbus-5");
-    let value: Value = client.wasm().query(anchor_overseer_address, query).await?;
-
-    match &value["result"]["prev_exchange_rate"] {
-        Value::String(v) => Ok(Decimal::from_str_radix(v, 10)?),
-        _ => Err(Box::new(IoError::new(
-            ErrorKind::Other,
-            "Parse error".to_string(),
-        ))),
-    }
-}
 
 pub async fn get_price(ids: &[&str]) -> Result<Vec<Decimal>, Box<dyn Error>> {
-    let mut result = fetch_price_from_coingecko(ids).await?;
-
-    // Query accurate anchor vprice from contract.
-    if let Some(idx) = ids.iter().position(|id| (*id).eq("anchorust")) {
-        result[idx] = fetch_price_aust().await?;
-    }
-    Ok(result)
+    Ok(fetch_price_from_coingecko(ids).await?)
 }
 
 #[cfg(test)]
@@ -66,7 +48,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_get_price() {
-        let prices = get_price(&["bitcoin", "ethereum", "anchorust"]).await.unwrap();
+        let prices = get_price(&["bitcoin", "ethereum", "shiba-inu"]).await.unwrap();
         assert_eq!(prices.len(), 3);
         assert!(prices[0] > Decimal::new(10000, 0));
         assert!(prices[1] > Decimal::new(1000, 0));
--- src/wallet.rs
+++ src/wallet.rs
@@ -9,8 +9,6 @@ use ethcontract_common::{hash, Abi};
 use num_traits::Pow;
 use rust_decimal::prelude::Decimal;
 use serde::Deserialize;
-use serde_json::{self, Value};
-use terra_rust_api::Terra;
 use web3::types::{Address, U256};
 use web3::Web3;
 
@@ -98,74 +96,6 @@ async fn substrate_get_balance(chain_id: &str, address: &str) -> Result<Decimal,
     }
 }
 
-async fn terra_get_coin_balance(coin: &str, address: &str) -> Result<Decimal, Box<dyn Error>> {
-    let client = Terra::lcd_client_no_tx("https://fcd.terra.dev", "columbus-5");
-    let result = client.bank().balances(address).await?;
-
-    let denom = match coin {
-        "UST" => "uusd",
-        "LUNA" => "uluna",
-        _ => {
-            return Err(Box::new(IoError::new(
-                ErrorKind::Other,
-                "invalid coin".to_string(),
-            )))
-        }
-    };
-
-    let mut amount = Decimal::new(1, 6);
-
-    for x in result.result.iter() {
-        if x.denom == denom {
-            amount *= x.amount;
-            break;
-        }
-    }
-    Ok(amount)
-}
-
-async fn terra_get_cw20_token_balance(
-    token_address: &str,
-    address: &str,
-) -> Result<Decimal, Box<dyn Error>> {
-    let client = Terra::lcd_client_no_tx("https://fcd.terra.dev", "columbus-5");
-    let query = "%7B%22token_info%22%3A%7B%7D%7D";
-
-    let value: Value = client.wasm().query(token_address, query).await?;
-    if let Value::Null = value["result"]["decimals"] {
-        return Err(Box::new(IoError::new(
-            ErrorKind::Other,
-            "query failed".to_string(),
-        )));
-    }
-    let decimals: u32 = value["result"]["decimals"].to_string().parse()?;
-
-    let query = format!(
-        "%7B%22balance%22%3A%7B%22address%22%3A%22{}%22%7D%7D",
-        address
-    );
-    let value: Value = client.wasm().query(token_address, query.as_str()).await?;
-    if let Value::Null = value["result"]["balance"] {
-        return Err(Box::new(IoError::new(
-            ErrorKind::Other,
-            "query failed".to_string(),
-        )));
-    }
-
-    let amount = match &value["result"]["balance"] {
-        Value::String(v) => Decimal::from_str_radix(v, 10)?,
-        _ => {
-            return Err(Box::new(IoError::new(
-                ErrorKind::Other,
-                "Parse balance result error".to_string(),
-            )))
-        }
-    };
-
-    let decimals = Pow::pow(Decimal::new(10, 0), decimals as u64);
-    Ok(amount / decimals)
-}
-
 pub async fn get_balance(
     chain_id: &str,
     token: Option<&str>,
@@ -177,19 +107,6 @@ pub async fn get_balance(
             None => web3_get_balance(chain_id, address).await,
         },
         "polkadot" => substrate_get_balance(chain_id, address).await,
-        "terra" => match token {
-            Some(token) => {
-                if token.len() > 4 {
-                    terra_get_cw20_token_balance(token, address).await
-                } else {
-                    terra_get_coin_balance(token, address).await
-                }
-            }
-            None => Err(Box::new(IoError::new(
-                ErrorKind::Other,
-                "no token specified".to_string(),
-            ))),
-        },
         _ => Err(Box::new(IoError::new(
             ErrorKind::Other,
             "invalid chain_id".to_string(),
@@ -238,28 +155,6 @@ mod tests {
             .await
             .unwrap();
         println!("DOT: {}", balance);
-        assert_ne!(Decimal::new(0, 0), balance);
-    }
-
-    #[tokio::test]
-    async fn test_terra_get_coin_balance() {
-        let balance =
-            terra_get_coin_balance("LUNA", "terra107q76k5uu3atgwz695vdcfee5qz9ukyz3jj0cs")
-                .await
-                .unwrap();
-        println!("LUNA: {}", balance);
-        assert_ne!(Decimal::new(0, 0), balance);
-    }
-
-    #[tokio::test]
-    async fn test_terra_get_cw20_token_balance() {
-        let balance = terra_get_cw20_token_balance(
-            "terra1hzh9vpxhsk8253se0vv5jj6etdvxu3nv8z07zu",
-            "terra107q76k5uu3atgwz695vdcfee5qz9ukyz3jj0cs",
-        )
-        .await
-        .unwrap();
-        println!("aUST: {}", balance);
         assert_ne!(Decimal::new(0, 0), balance);
     }
 }
